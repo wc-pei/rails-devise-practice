f83e404728ee2acd71aa8af1d7399607e7307bb8
wc-pei <liming.pei@webcrew.co.jp>
2023-12-05 01:15:58 +0900

    fix: rails7 turbo frameのdevise設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+#https://github.com/heartcombo/devise#strong-parameters
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # /users/sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name])
+  end
 end
